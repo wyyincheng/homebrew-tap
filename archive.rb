db0c8893dfbface355408d95337a1730498a9c19
cheng.yin <cheng_yin@jiliguala.com>
2020-05-20 14:56:53 +0800

    update version

--- archive.rb
+++ archive.rb
@@ -4,9 +4,9 @@
 class Archive < Formula
     desc ""
     homepage ""
-    version = 'v0.0.3'
+    version = '0.0.7'
     url "https://github.com/wyyincheng/archive/releases/download/v0.0.3/archive_0.0.3_darwin_amd64.tar.gz"
-    sha256 "d1cf052ba477237a7646648cd9f5e93a530309350eba7a50f6e136cf1ed01788"
+    sha256 "13f2aa474e2a5dc5691ea870fc582075f0f36dcbb0bb75e3673a9e300d8e407d"
   
     def install
       bin.install "archive"
