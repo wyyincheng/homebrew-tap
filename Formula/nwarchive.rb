ecd4d7ba79cf012666da7757e36bdb9e545d3c3f
cheng.yin <cheng_yin@jiliguala.com>
2020-06-30 09:36:17 +0800

    update version

--- Formula/nwarchive.rb
+++ Formula/nwarchive.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class NWArchive < Formula
+class Nwarchive < Formula
     desc ""
     homepage ""
     version = '0.0.1'
